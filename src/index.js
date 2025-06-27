f007ac37fb94fd2daf844dff599bfea6b1b7dff5
joanaferreira80 <joanaferreira.br@gmail.com>
2025-06-27 15:39:41 +0100

    add js forecast

--- src/index.js
+++ src/index.js
@@ -15,22 +15,6 @@ windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 iconElement.innerHTML= `<img src="${response.data.condition.icon_url}"class="current-temperature-icon"/>`;
 
 }
-
-function searchCity (city){
-  let apiKey = "t61c0cb24c8co6107fad0a63023aa3ea";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-}
-
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
- searchCity(searchInputElement.value);
-}
-searchCity("London");
-
-
-
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -56,14 +40,43 @@ function formatDate(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
 
+function searchCity (city){
+  let apiKey = "t61c0cb24c8co6107fad0a63023aa3ea";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+ searchCity(searchInputElement.value);
+}
 
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml="";
+  days.forEach(function (day) {
+    forecastHtml = forecastHtml +
+    `<div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">☀️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature"><strong>15°</strong></div>
+          <div class="weather-forecast-temperature">8°</div>
+        </div>
+      </div>`;
+           });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+searchCity("London");
+displayForecast();
